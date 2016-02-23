1eeb7ec7caaebd64488257b903f76c9dab8417e5
Kovalev Evgeniy <e.evilgen@gmail.com>
2016-02-23 18:30:34 +0300

    Add visit.erb and connect java plugins

--- app.rb
+++ app.rb
@@ -14,8 +14,29 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do
+	@barbers = Barber.all
+end
 
 get '/' do
-	@barbers = Barber.all
+
 	erb :index			
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@master = params[:master]
+	@color = params[:colorpicker]
+
+
+
+@message = "Thank you #{@username}, we will wait for you."
+
+erb "#{@message}"
 end
