4e9d79aba43989f2d0263073e66d82dbc907262f
Kovalev Evgeniy <e.evilgen@gmail.com>
2016-02-23 15:19:32 +0300

    Add Clietn entity

--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinattra/activerecord'
+
+#Создание подключения к БД при помощи Active Record
+set :database, "sqlite3:EKBarberShop.db"
+
+class Client < ActiveRecord::Base
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
