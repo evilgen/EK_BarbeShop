d54bf5c4265bd6e2cfd7b76b84c8833b101d2cf9
Kovalev Evgeniy <e.evilgen@gmail.com>
2016-02-24 12:06:18 +0300

    Add comments

--- app.rb
+++ app.rb
@@ -11,26 +11,33 @@ set :database, "sqlite3:EKBarberShop.db"
 class Client < ActiveRecord::Base
 end
 
+#Создание сущности Парикмахер с наследованием
 class Barber < ActiveRecord::Base
 end
 
+#Создание сущности Контакт с наследованием
 class Contact < ActiveRecord::Base
 end
 
 
+#before вызывается каждый раз при перезагрузке
+#любой страницы
 before do
 	@barbers = Barber.all
 end
 
 get '/' do
-
 	erb :index			
 end
 
+#обработчик get-запроса /visit
+#браузер получает страницу с сервера
 get '/visit' do
 	erb :visit
 end
 
+#обработчие post-запроса /visit
+#браузер отправляет страницу на сервер
 post '/visit' do
 	@username = params[:username]
 	@phone = params[:phone]
@@ -38,7 +45,7 @@ post '/visit' do
 	@master = params[:master]
 	@color = params[:colorpicker]
 
-
+#Сохранение данных в базе данных
 Client.create 	:name=>@username,
  				:phone=>@phone, 
 				:datestamp=>@datetime,
@@ -50,19 +57,23 @@ Client.create 	:name=>@username,
 erb "#{@message}"
 end
 
+#обработчик get-запроса /contacts
+#браузер получает страницу с сервера
 get '/contacts' do
 	erb :contacts
 end
 
+#обработчие post-запроса /contacts
+#браузер отправляет страницу на сервер
 post '/contacts' do
 	@email = params[:email]
 	@letter = params[:letter]
 
+#Сохранение данных в базе данных
 Contact.create 	:email=>@email,
  				:letter=>@letter
 	
-	@message = "Thank you for your message."
+@message = "Thank you for your message."
 
-	erb "#{@message}"
-	
+erb "#{@message}"
 end
